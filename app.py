20bf009209417257a5ceccb38f696514b83d5747
fdeboo <frances.deboo@gmail.com>
2020-04-21 12:44:04 +0100

    files installed

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists('env.py'):
+    import env
 
 app = Flask(__name__)
 
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["MONGO_URI"] = "mongodb+srv://root:patch2488@mycluster-qtjor.mongodb.net/task_manager?retryWrites=true&w=majority"
+os.environ["SECRET_KEY"] = "583584"
